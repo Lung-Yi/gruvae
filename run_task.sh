f51a035215cd760b6a9e35ee6c973de6f7595d59
Lung-Yi <sun73912@gmail.com>
2025-12-10 00:11:03 +0800

    Rename data

---
+++ run_task.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 1. 執行 Python，並把輸出寫入 log
+# (這裡不需要 nohup，因為我們會在執行這個腳本時加 nohup)
+python train.py > my_log.com 2>&1
+
+# 2. 執行 Git 推送
+git add my_log.com
+git commit -m "Auto update: log file after execution"
+git push
